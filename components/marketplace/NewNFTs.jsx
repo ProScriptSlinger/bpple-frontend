faab72787c0e9092d3e068bb32e2fe09ff1f652a
ProScriptSlinger <stanely.davis.941105@gmail.com>
2024-06-07 06:34:49 +0900

    feat: nft image

--- components/marketplace/NewNFTs.jsx
+++ components/marketplace/NewNFTs.jsx
@@ -29,7 +29,7 @@ const NewNFTs = (props) => {
           width={0}
           height={0}
           alt=""
-          className="w-full aspect-[4/3] rounded-[18px] max-h-[300px]"
+          className="w-full aspect-[4/3] rounded-[18px] max-h-[150px]"
         />
         <div className="w-full h-[30%] bg-black absolute bottom-0 rounded-[18px] bg-opacity-25 backdrop-blur-[10px] flex items-center justify-center px-[20px]">
           <div className="w-full inline-flex justify-between items-center">
